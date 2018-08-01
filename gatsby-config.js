1ae6a70b7e475afcd60edc030f01eecfe8d16efb
Matt Morgan <mlmorg@uber.com>
2018-08-01 09:30:16 -0700

    Update GA account

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-7157694-88',
+        trackingId: 'UA-123210196-1',
       },
     },
   ],

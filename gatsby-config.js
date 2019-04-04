df97ff5d19d0f9cff0a0b3a96b2e48347a746e8f
Derek Ju <derekju@uber.com>
2019-04-04 13:38:52 -0700

    Update getting started section

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,20 @@ module.exports = {
           'gatsby-remark-transform-links',
           'gatsby-remark-autolink-headers',
           {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              ignoreFileExtensions: [],
+            },
+          },
+          {
             resolve: 'gatsby-remark-prismjs',
             options: {
               // Class prefix for <pre> tags containing syntax highlighting;

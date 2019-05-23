5baf71747106b69a97d7607c10e4897785daf78c
Kevin Grandon <kevingrandon@yahoo.com>
2019-05-23 14:56:39 -0700

    Update doc generation to pull from monorepo
    https://github.com/fusionjs/fusionjs.github.io/pull/131

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,14 @@ dest=fusion
 rm -rf $dest
 mkdir -p $dest
 
+git clone https://github.com/fusionjs/fusionjs.git
+
 while read -r repo
 do
-  git clone https://github.com/fusionjs/$repo.git $dest/$repo
+  mv fusionjs/$repo $dest/$repo
 done < $oss
+
+rm -rf fusionjs
 
 # Custom handling for fusionjs.github.io
 mkdir -p fusion/fusion-docs
